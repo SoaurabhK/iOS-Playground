052fb7f5ce996fc2195179b0db91276f1f6a5c6a
Soaurabh Kakkar <soaurabh.kakkar@monotype.com>
2016-06-04 22:55:08 +0530

    Working with Arrays And Dictionaries

--- USK-Playground.playground/Contents.swift
+++ USK-Playground.playground/Contents.swift
@@ -38,6 +38,86 @@ print(number * Double(int)) //NOTE-Arithmatic of only same type of variables is 
 //Booleans(true or false)
 var isMale:Bool = true //Bool allows true/false, named after George Boole
 
+//Working with Arrays And Dictionaries
+
+//Arrays
+var array:[String] = ["My", "Name", "Is", "Soaurabh", "Kakkar"]
+
+var arrInts : [Int] = [17, 25, 13, 47]
+
+var arr = [1, 2, 3, 4, "Soaurabh"] // it will become array of NSObject in this case, but swift array has the same type of variables.
+
+print(arrInts[0]) //swift array is zero based
+print(arrInts.count) //Number of items in the array
+
+arrInts.append(56) // Add the element at the end of the array
+
+arrInts.removeAtIndex(3) //Removes the element at index, do shifting and returns the removed element
+
+//arrInts.removeAtIndex(4) - Error, Trying to remove an item which is out of bound
+
+print(arrInts)
+
+arrInts.sort()
+
+
+//Dictionaries
+
+var dict:[String:String] = ["Name" : "Soaurabh", "Computer":"something to play Call of Duty on", "Coffee" : "Best Drink Ever"] //key-value pair
+
+if let valOfDict = dict["Coffee"] { // Note- Dict[key] returns optionals
+    print(valOfDict)
+}
+
+print(dict.count) //Number of key-value pairs
+
+dict["pen"] = "Write it with pen" //Adding values to dictionary
+
+dict.removeValueForKey("Computer") //Returns the value which gets removed
+
+var menu = ["pizza": 10.99, "ice cream": 4.99, "salad": 7.99]
+
+var totalCost:Double = 0.0;
+for (key, value) in menu { //Iterating a dictionary
+    totalCost += value
+}
+
+//print("The total cost of the tree items is \(menu["pizza"]! + menu["ice cream"]! + menu["salad"]!)") - Not recommended to force unwrap variables
+
+print("The total cost of the tree items is \(totalCost)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
