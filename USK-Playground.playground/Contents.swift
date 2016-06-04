fe7b3407ef6a2b2d193536513b8f73b2d0c7947f
Soaurabh Kakkar <soaurabh.kakkar@monotype.com>
2016-06-04 21:53:28 +0530

    Working with variables and variable types

--- USK-Playground.playground/Contents.swift
+++ USK-Playground.playground/Contents.swift
@@ -4,4 +4,63 @@ import UIKit
 
 var str = "Hello, playground"
 
-var st = "g"
+print("Printing on the console")
+
+//Working with variables and variable types
+
+//Strings
+let name : String = "Soaurabh Kakkar" //it's a good habit to define your variable's type but not necessary
+
+print("My name is \(name)")
+
+print("Hello " + name + ".") //string concat with plus
+
+//Integers(Whole Numbers)
+var int : Int = 9
+
+int = int * 2     //int is 18
+
+int = int / 4    // rounds down to 4
+
+var anotherInt = 7
+
+print(int * anotherInt)
+
+print("The value of int is \(int)")
+
+//Doubles(numbers with decimals)
+let number:Double = 4.53 //decimal numbers default to Double type
+
+print(number * 7)
+
+print(number * Double(int)) //NOTE-Arithmatic of only same type of variables is possible
+
+//Booleans(true or false)
+var isMale:Bool = true //Bool allows true/false, named after George Boole
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
